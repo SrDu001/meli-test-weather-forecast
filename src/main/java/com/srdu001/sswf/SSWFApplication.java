6237d719805e295b545f29ce2ca640ff5e9d4c90
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-07-25 16:36:04 -0500

    add swagger

--- src/main/java/com/srdu001/sswf/SSWFApplication.java
+++ src/main/java/com/srdu001/sswf/SSWFApplication.java
@@ -1,9 +1,15 @@
 package com.srdu001.sswf;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info= @Info(
+        title = "Solar System Weather Forecast API",
+        version = "1.0",
+        description = "Weather forecast for the fictional star trek-based Solar system"))
 public class SSWFApplication {
 
     public static void main(String[] args) {
